a24def50af61d3defa4200dd82379f8bb7960181
JunYoung CHoi <juny0955@gmail.com>
2025-05-29 23:19:01 +0900

    fix: 좌석 예약 API Swagger 설명 추가 #3

--- src/main/java/kr/hhplus/be/server/interfaces/api/concert/ConcertController.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/concert/ConcertController.java
@@ -56,6 +56,41 @@ public class ConcertController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
+	@Operation(
+		summary = "콘서트 좌석 예약 API",
+		description = "콘서트 좌석 예약"
+	)
+	@ApiResponses(value = {
+		@ApiResponse(
+			responseCode = "200",
+			description = "예약 성공",
+			content = @Content(schema = @Schema(implementation = ReservationResponse.class))
+		),
+		@ApiResponse(
+			responseCode = "404 - User",
+			description = "유저 찾을 수 없음"
+		),
+		@ApiResponse(
+			responseCode = "404 - Concert",
+			description = "콘서트 찾을 수 없음"
+		),
+		@ApiResponse(
+			responseCode = "404 - Seat",
+			description = "좌석 찾을 수 없음"
+		),
+		@ApiResponse(
+			responseCode = "400",
+			description = "좌석이 AVAILABLE 상태가 아님"
+		),
+		@ApiResponse(
+			responseCode = "401",
+			description = "토큰 만료 등 토큰이 잘못됨"
+		),
+		@ApiResponse(
+			responseCode = "409",
+			description = "락 획득 실패 (다른 사용자 점유중)"
+		)
+	})
 	@PostMapping("/{concertId}/reservations")
 	public ResponseEntity<ReservationResponse> reservationConcert(
 		@PathVariable UUID concertId,
