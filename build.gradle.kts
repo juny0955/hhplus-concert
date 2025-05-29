f04add02a83b83288839449e296039d5fba07a44
JunYoung CHoi <juny0955@gmail.com>
2025-05-29 22:13:08 +0900

    setting: swagger 설정 #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
-    // DB
+	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
     // Test
