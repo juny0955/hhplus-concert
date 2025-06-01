fff64b07d3c34233dda6164512bd0b238200dea8
JunYoung CHoi <juny0955@gmail.com>
2025-06-01 18:42:40 +0900

    JWT dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,11 @@ dependencies {
 	implementation("org.projectlombok:lombok")
 	annotationProcessor ("org.projectlombok:lombok")
 
+	// jwt
+	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
 	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
